b2f6589731ca9b4a1dc7d1df11ef27cdc9d67970
Ghilain <ishimweghilain5050@gmail.com>
2023-01-26 20:41:21 +0200

    for loop used for colums

--- challenge.js
+++ challenge.js
@@ -3,9 +3,14 @@
  * @return {boolean}
  */
 var isValidSudoku = function(board) {
+    // decrale variables 
     const found = new Set();
     let block;
+    // decrale loop used for row
     for (let i=0; i<9; ++i) {
+        // decrale loop for columns
+        for (let j=0; j<9; ++j) {
 
+        }
     }
 };
