b7699313278004b8603213be21da0f2cf1038016
Ghilain <ishimweghilain5050@gmail.com>
2023-01-26 20:43:26 +0200

    add if condition to check if number is not char

--- challenge.js
+++ challenge.js
@@ -6,11 +6,16 @@ var isValidSudoku = function(board) {
     // decrale variables 
     const found = new Set();
     let block;
-    // decrale loop used for row
+    // decrale loop used for rows
     for (let i=0; i<9; ++i) {
         // decrale loop for columns
         for (let j=0; j<9; ++j) {
+            // decrale variable number
+            let number = board[i][j];
+            // add if condition to check if number not charcter
+            if (number != '.') {
 
+            }
         }
     }
 };
